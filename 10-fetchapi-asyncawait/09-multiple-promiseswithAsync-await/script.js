ed622fd88d94537e58bd8416596cd97708547d90
mirzakarabulut <mirzakara1234@gmail.com>
2024-03-23 14:23:25 +0300

    Remove clear items functionality and clear all button. (generated by blackbox.ai )

--- 10-fetchapi-asyncawait/09-multiple-promiseswithAsync-await/script.js
+++ 10-fetchapi-asyncawait/09-multiple-promiseswithAsync-await/script.js
@@ -1,32 +1,28 @@
 function getData(endpoint) {
   return new Promise((resolve, reject) => {
-    
-      const xhr = new XMLHttpRequest();
-      xhr.open('GET', endpoint);
-    
-      xhr.onreadystatechange = () => {
-        if (xhr.readyState === 4) {
-          
-          if (xhr.status === 200){
-              resolve(JSON.parse(xhr.responseText));
-          } else {
-            reject('Something went wrong')
-            }
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", endpoint);
+
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+          resolve(JSON.parse(xhr.responseText));
+        } else {
+          reject("Something went wrong");
         }
-        }
-        setTimeout(() => {
-          xhr.send();
-        }, Math.floor(Math.random() * 3000) + 1000);
+      }
+    };
+    setTimeout(() => {
+      xhr.send();
+    }, Math.floor(Math.random() * 3000) + 1000);
   });
 }
 
 async function getAllData() {
-  const movies = await getData('./movies.json');
-  const directors = await getData('./directors.json');
-  const actors = await getData('./actors.json');
-  console.log(movies,directors,actors);
+  const movies = await getData("./movies.json");
+  const directors = await getData("./directors.json");
+  const actors = await getData("./actors.json");
+  console.log(movies, directors, actors);
 }
 
 getAllData();
-
-
